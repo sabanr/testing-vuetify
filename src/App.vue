e2ac87413b93f974d7e43db215b4514437ea8c6d
Roberto Saban <roberto.saban@sweet.com.ar>
2019-04-03 19:57:32 -0300

    created toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+    <nav-bar></nav-bar>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -7,8 +8,13 @@
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue'
+
 export default {
   name: 'App',
+  components: {
+    NavBar
+  },
   data () {
     return {
       //

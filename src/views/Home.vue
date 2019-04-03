712fb6f545ff9318ba16391e8c7a5e7fc2e8a324
Roberto Saban <roberto.saban@sweet.com.ar>
2019-04-03 00:03:27 -0300

    starting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,3 @@
 <template>
-  <HelloWorld />
+  <div><h1>Home Page</h1></div>
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>

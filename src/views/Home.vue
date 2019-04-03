ef4ab45a69b4bdabaea06ca6955190e79324593e
Roberto Saban <roberto.saban@sweet.com.ar>
2019-04-03 00:46:59 -0300

    buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,25 @@
 <template>
-  <div><h1>Home Page</h1></div>
+  <div class="home">
+    <h1>Homepage</h1>
+    
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="pink">Click me</v-btn>
+    <v-btn flat color="pink">Click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+    <v-btn small depressed class="pink white--text">
+      <v-icon small left>email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+    <v-btn large depressed class="pink white--text">
+      <span>EMAIL ME</span>
+      <v-icon large right>email</v-icon>
+    </v-btn>
+    <v-btn fab small dark depressed color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+  </div>
 </template>

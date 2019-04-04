7b3334c101fd4f428172f3137e91c8f7ec31c862
Roberto Saban <roberto.saban@sweet.com.ar>
2019-04-03 21:01:28 -0300

    navigation drawer with amazing list inside

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,8 +12,19 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
-        <v-navigation-drawer v-model="drawer" app class="indigo">
-            <p>test</p>
+        <v-navigation-drawer v-model="drawer" app class="primary">
+            <v-list>
+                <v-list-tile v-for="(link, index) in links" :key="index" router :to="link.route">
+                    <v-list-tile-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">
+                            {{ link.text }}
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
         </v-navigation-drawer>
 
     </nav>
@@ -23,7 +34,12 @@
 export default {
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'folder', text: 'My projects', route: '/projects' },
+                { icon: 'person', text: 'Team', route: '/team' },
+            ]
         }
     }
 }
